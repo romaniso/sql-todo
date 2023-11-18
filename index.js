fdc13a52723ad69cc8f495d99cd73bb1ef928a09
Roman Isopenko <isopenkoroman@gmail.com>
2023-11-18 13:19:06 +0100

    add config files, install express, express-handlebars for routing

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+const express = require("express");
+const {join} = require("path");
+const {engine} = require('express-handlebars');
+const {homeRouter} = require("./routes/home")
 const {pool} = require("./utils/db");
 const {TodoRecord} = require("./records/todo.record");
 
-(async() => {
-    const foundTodo = await TodoRecord.find('a56546bb-064c-42de-b85a-dcb4715697ef');
-    foundTodo.title = 'A może coś innego porobimy, co?';
-    await foundTodo.update();
-    console.log(foundTodo);
-    await pool.end();
-})()
+// (async() => {
+//     const foundTodo = await TodoRecord.find('a56546bb-064c-42de-b85a-dcb4715697ef');
+//     foundTodo.title = 'A może coś innego porobimy, co?';
+//     await foundTodo.update();
+//     console.log(foundTodo);
+//     await pool.end();
+// })()
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', join(__dirname,'/views'));
+
+app.use('/', homeRouter);
+
+app.listen(3000, 'localhost', () => {
+    console.log("Listening on http://localhost:3000");
+});
