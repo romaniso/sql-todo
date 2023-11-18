b9ff71db42f7e4e3c6f7d191676d9f3e1334721a
Roman Isopenko <isopenkoroman@gmail.com>
2023-11-18 15:05:00 +0100

    feat addition, add goback partial and added page

--- routes/home.js
+++ routes/home.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const homeRouter = express.Router();
+const bodyParser = require('body-parser');
 const {pool} = require("../utils/db");
 const {TodoRecord} = require("../records/todo.record");
 const {TodoRepository} = require("../repositories/todo.repository");
 
-
-// (async() => {
-//     const foundTodo = await TodoRecord.find('a56546bb-064c-42de-b85a-dcb4715697ef');
-//     foundTodo.title = 'A może coś innego porobimy, co?';
-//     await foundTodo.update();
-//     console.log(foundTodo);
-//     await pool.end();
-// })()
+// Middlewares
+homeRouter.use(bodyParser.urlencoded({ extended: false }));
 
 // Here must be REST logics
 homeRouter
     .get('/', async (req, res) => {
         const foundTodos = await TodoRepository.findAll();
-        console.log(foundTodos);
         // await pool.end();
         res.render('home', {
             todos: foundTodos,
@@ -28,6 +22,11 @@ homeRouter
         const todoItemToRemove = await TodoRepository.find(id);
         await TodoRepository.delete(todoItemToRemove);
         res.render('delete');
+    })
+    .post('/added', async (req, res) => {
+        const newTodo = new TodoRecord({title: req.body.title});
+        await TodoRepository.insert(newTodo);
+        res.render('added');
     })
 
 module.exports = {
